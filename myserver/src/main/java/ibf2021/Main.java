9d81c059f2177731790aa7c7e5e765bcb51afca7
LeoM10-Git <cc20082012@yahoo.com.sg>
2021-12-14 17:09:26 +0800

    Latest updated

--- myserver/src/main/java/ibf2021/Main.java
+++ myserver/src/main/java/ibf2021/Main.java
@@ -15,12 +15,13 @@ public class Main {
         if (args != null && args.length > 0) {
             if (args.length > 1 && "--port".equals(args[0])) {
                 userPort = args[1];
-                HttpServer httpServer = new HttpServer(userPort, "static");
+                HttpServer httpServer = new HttpServer(userPort, "static/");
                 httpServer.startServer();
             } else if (args.length > 1 && "--docRoot".equals(args[0])) {
                 String[] listDir = args[1].split(":");
                 for (String dir : listDir) {
                     if (checkFile(dir)) {
+                        dir = dir +"/";
                         HttpServer httpServer = new HttpServer(userPort, dir);
                         httpServer.startServer();
                         break;
